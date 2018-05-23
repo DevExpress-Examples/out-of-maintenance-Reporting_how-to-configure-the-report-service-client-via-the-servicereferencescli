d20fe3c40131e9ecc7c27d0396fbb5915a8961ec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 17:53:01 +0300

    initial commit

---
+++ CS/E2876/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using DevExpress.DocumentServices.ServiceModel.Client;
+using DevExpress.Xpf.Printing;
+
+namespace E2876 {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            preview.Model = new ReportPreviewModel() {
+                ReportName = "E2876.Web.SampleReport, E2876.Web",
+                ServiceClientFactory = 
+                    new ReportServiceClientFactory("endpointConfiguration")
+            };
+        }
+    }
+}
